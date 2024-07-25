03e29165c4b0c0fc3058adff3a09628774d23c09
TheTurtle <47210458+raphaelthegreat@users.noreply.github.com>
2024-07-25 23:54:31 +0200

    Migrate to tracy profiler from Microprofile

--- src/citra_qt/main.h
+++ src/citra_qt/main.h
@@ -38,7 +38,6 @@ class GRenderWindow;
 class IPCRecorderWidget;
 class LLEServiceModulesWidget;
 class LoadingScreen;
-class MicroProfileDialog;
 class MultiplayerState;
 class ProfilerWidget;
 class QFileOpenEvent;
@@ -351,7 +350,6 @@ private:
 
     // Debugger panes
     ProfilerWidget* profilerWidget;
-    MicroProfileDialog* microProfileDialog;
     RegistersWidget* registersWidget;
     GPUCommandStreamWidget* graphicsWidget;
     GPUCommandListWidget* graphicsCommandsWidget;
