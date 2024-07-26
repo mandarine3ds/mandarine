d865fce7832614ec24b99921dd034c44a3157ae9
BlakDulz <3345247+blakbin@users.noreply.github.com>
2024-07-26 03:52:34 +0200

    PC: Implement Layout settings tab and support
    Co-Authored-By: Reg Tiangha <8913195+rtiangha@users.noreply.github.com>
    Co-Authored-By: OpenSauce <48618519+opensauce04@users.noreply.github.com>

--- src/video_core/renderer_opengl/renderer_opengl.cpp
+++ src/video_core/renderer_opengl/renderer_opengl.cpp
@@ -694,7 +694,9 @@ void RendererOpenGL::DrawScreens(const Layout::FramebufferLayout& layout, bool f
 }
 
 void RendererOpenGL::ApplySecondLayerOpacity() {
-    if (Settings::values.custom_layout &&
+#ifndef ANDROID // TODO: Implement custom layouts on Android
+    if ((Settings::values.layout_option.GetValue() == Settings::LayoutOption::CustomLayout ||
+         Settings::values.custom_layout) &&
         Settings::values.custom_second_layer_opacity.GetValue() < 100) {
         state.blend.src_rgb_func = GL_CONSTANT_ALPHA;
         state.blend.src_a_func = GL_CONSTANT_ALPHA;
@@ -702,10 +704,13 @@ void RendererOpenGL::ApplySecondLayerOpacity() {
         state.blend.dst_rgb_func = GL_ONE_MINUS_CONSTANT_ALPHA;
         state.blend.color.alpha = Settings::values.custom_second_layer_opacity.GetValue() / 100.0f;
     }
+#endif
 }
 
 void RendererOpenGL::ResetSecondLayerOpacity() {
-    if (Settings::values.custom_layout &&
+#ifndef ANDROID // TODO: Implement custom layouts on Android
+    if ((Settings::values.layout_option.GetValue() == Settings::LayoutOption::CustomLayout ||
+         Settings::values.custom_layout) &&
         Settings::values.custom_second_layer_opacity.GetValue() < 100) {
         state.blend.src_rgb_func = GL_ONE;
         state.blend.dst_rgb_func = GL_ZERO;
@@ -713,6 +718,7 @@ void RendererOpenGL::ResetSecondLayerOpacity() {
         state.blend.dst_a_func = GL_ZERO;
         state.blend.color.alpha = 0.0f;
     }
+#endif
 }
 
 void RendererOpenGL::DrawTopScreen(const Layout::FramebufferLayout& layout,
