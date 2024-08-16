69f6dd346b5fff790a4a7b8875d19a665b6e35a0
Gamer64 <76565986+Gamer64ytb@users.noreply.github.com>
2024-08-16 11:13:25 +0200

    Android: Specify app ID on about game dialog

--- src/android/app/src/main/java/io/github/mandarine3ds/mandarine/adapters/GameAdapter.kt
+++ src/android/app/src/main/java/io/github/mandarine3ds/mandarine/adapters/GameAdapter.kt
@@ -203,7 +203,7 @@ class GameAdapter(private val activity: AppCompatActivity, private val inflater:
 
         bottomSheetView.findViewById<TextView>(R.id.about_game_title).text = game.title
         bottomSheetView.findViewById<TextView>(R.id.about_game_company).text = game.company
-        bottomSheetView.findViewById<TextView>(R.id.about_game_id).text = String.format("%016X", game.titleId)
+        bottomSheetView.findViewById<TextView>(R.id.about_game_id).text = String.format("ID: %016X", game.titleId)
         GameIconUtils.loadGameIcon(activity, game, bottomSheetView.findViewById(R.id.game_icon))
 
         bottomSheetView.findViewById<MaterialButton>(R.id.about_game_play).setOnClickListener {
