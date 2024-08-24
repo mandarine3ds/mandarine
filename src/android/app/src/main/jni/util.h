2b842982049284ebe0cbcb0897d4971c5c857be6
OpenSauce04 <opensauce04@gmail.com>
2024-08-24 21:43:11 +0200

    Fixed swapping screens on landscape displays forcing portrait

---
+++ src/android/app/src/main/jni/util.h
@@ -0,0 +1,8 @@
+// Copyright 2024 Lime3DS Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+/// Calls and returns the value of NativeLibrary.isPortraitMode
+bool IsPortraitMode();
