rtiangha/bravely-offline-citra

Create SpacingItemDecoration.kt
credits: strato

src/android/app/src/main/java/io/github/borked3ds/android/adapters/SpacingItemDecoration.kt
@@ -1,0 +1,14 @@
+
+package io.github.borked3ds.android
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class SpacingItemDecoration(private val padding : Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(outRect : Rect, view : View, parent : RecyclerView, state : RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.set(0, 0, 0, padding)
+    }
+}

mandarine3ds/mandarine

src/android/app/src/main/java/io/github/mandarine3ds/mandarine/adapters/SpacingItemDecoration.kt
@@ -1,0 +1,14 @@
+
+package io.github.mandarine3ds.mandarine
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class SpacingItemDecoration(private val padding : Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(outRect : Rect, view : View, parent : RecyclerView, state : RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.set(0, 0, 0, padding)
+    }
+}