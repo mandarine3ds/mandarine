rtiangha/bravely-offline-citra

Create HomeSettingStringUtils.kt

src/android/app/src/main/java/io/github/borked3ds/android/utils/HomeSettingStringUtils.kt
@@ -1,0 +1,7 @@
+
+package io.github.borked3ds.android.utils
+
+sealed class HomeSettingStringUtils {
+    data class Text(val value: String) : HomeSettingStringUtils()
+    data class ResId(val id: Int) : HomeSettingStringUtils()
+}

mandarine3ds/mandarine

src/android/app/src/main/java/io/github/mandarine3ds/mandarine/utils/HomeSettingStringUtils.kt
@@ -1,0 +1,7 @@
+
+package io.github.mandarine3ds.mandarine.utils
+
+sealed class HomeSettingStringUtils {
+    data class Text(val value: String) : HomeSettingStringUtils()
+    data class ResId(val id: Int) : HomeSettingStringUtils()
+}