Lime3DS/Lime3DS

android: Add screen orientation presets to layout settings (#420)
Co-authored-by: OpenSauce04 <opensauce04@gmail.com>

src/android/app/src/main/java/io/github/lime3ds/android/activities/EmulationActivity.kt
@@ -34,6 +34,7 @@ import io.github.lime3ds.android.databinding.ActivityEmulationBinding
 import io.github.lime3ds.android.display.ScreenAdjustmentUtil
 import io.github.lime3ds.android.features.hotkeys.HotkeyUtility
 import io.github.lime3ds.android.features.settings.model.BooleanSetting
+import io.github.lime3ds.android.features.settings.model.IntSetting
 import io.github.lime3ds.android.features.settings.model.SettingsViewModel
 import io.github.lime3ds.android.features.settings.model.view.InputBindingSetting
 import io.github.lime3ds.android.fragments.EmulationFragment
@@ -75,7 +76,7 @@ class EmulationActivity : AppCompatActivity() {
         NativeLibrary.enableAdrenoTurboMode(BooleanSetting.ADRENO_GPU_BOOST.boolean)
 
         binding = ActivityEmulationBinding.inflate(layoutInflater)
-        screenAdjustmentUtil = ScreenAdjustmentUtil(windowManager, settingsViewModel.settings)
+        screenAdjustmentUtil = ScreenAdjustmentUtil(this, windowManager, settingsViewModel.settings)
         hotkeyUtility = HotkeyUtility(screenAdjustmentUtil)
         setContentView(binding.root)
 
@@ -105,6 +106,8 @@ class EmulationActivity : AppCompatActivity() {
 
         isEmulationRunning = true
         instance = this
+
+        applyOrientationSettings() // Check for orientation settings at startup
     }
 
     // On some devices, the system bars will not disappear on first boot or after some
@@ -113,6 +116,7 @@ class EmulationActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         enableFullscreenImmersive()
+        applyOrientationSettings() // Check for orientation settings changes on runtime
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
@@ -202,6 +206,11 @@ class EmulationActivity : AppCompatActivity() {
             controller.systemBarsBehavior =
                 WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
+    }
+
+    private fun applyOrientationSettings() {
+        val orientationOption = IntSetting.ORIENTATION_OPTION.int
+        screenAdjustmentUtil.changeActivityOrientation(orientationOption)
     }
 
     // Gets button presses

src/android/app/src/main/java/io/github/lime3ds/android/display/ScreenAdjustmentUtil.kt
@@ -4,16 +4,21 @@
 
 package io.github.lime3ds.android.display
 import android.view.WindowManager
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.app.Activity
 import io.github.lime3ds.android.NativeLibrary
 import io.github.lime3ds.android.features.settings.model.BooleanSetting
 import io.github.lime3ds.android.features.settings.model.IntSetting
 import io.github.lime3ds.android.features.settings.model.Settings
 import io.github.lime3ds.android.features.settings.utils.SettingsFile
 import io.github.lime3ds.android.utils.EmulationMenuSettings
+import io.github.lime3ds.android.R
 
 class ScreenAdjustmentUtil(
+    private val context: Context,
     private val windowManager: WindowManager,
-    private val settings: Settings
+    private val settings: Settings,
 ) {
     fun swapScreen() {
         val isEnabled = !EmulationMenuSettings.swapScreens
@@ -26,21 +31,20 @@ class ScreenAdjustmentUtil(
         settings.saveSetting(BooleanSetting.SWAP_SCREEN, SettingsFile.FILE_NAME_CONFIG)
     }
     fun cycleLayouts() {
-        // TODO: figure out how to pull these from R.array
-        val landscape_values = intArrayOf(2,1,3,4,0,5);
-        val portrait_values = intArrayOf(0,1);
-        if (NativeLibrary.isPortraitMode) {
-            val current_layout = IntSetting.PORTRAIT_SCREEN_LAYOUT.int
-            val pos = portrait_values.indexOf(current_layout)
-            val layout_option = portrait_values[(pos + 1) % portrait_values.size]
-            changePortraitOrientation(layout_option)
-        } else {
-            val current_layout = IntSetting.SCREEN_LAYOUT.int
-            val pos = landscape_values.indexOf(current_layout)
-            val layout_option = landscape_values[(pos + 1) % landscape_values.size]
-            changeScreenOrientation(layout_option)
-        }
+        val landscapeValues = context.resources.getIntArray(R.array.landscapeValues)
+        val portraitValues = context.resources.getIntArray(R.array.portraitValues)
 
+        if (NativeLibrary.isPortraitMode) {
+            val currentLayout = IntSetting.PORTRAIT_SCREEN_LAYOUT.int
+            val pos = portraitValues.indexOf(currentLayout)
+            val layoutOption = portraitValues[(pos + 1) % portraitValues.size]
+            changePortraitOrientation(layoutOption)
+        } else {
+            val currentLayout = IntSetting.SCREEN_LAYOUT.int
+            val pos = landscapeValues.indexOf(currentLayout)
+            val layoutOption = landscapeValues[(pos + 1) % landscapeValues.size]
+            changeScreenOrientation(layoutOption)
+        }
     }
 
     fun changePortraitOrientation(layoutOption: Int) {
@@ -55,5 +59,12 @@ class ScreenAdjustmentUtil(
         settings.saveSetting(IntSetting.SCREEN_LAYOUT, SettingsFile.FILE_NAME_CONFIG)
         NativeLibrary.reloadSettings()
         NativeLibrary.updateFramebuffer(NativeLibrary.isPortraitMode)
+    }
+
+    fun changeActivityOrientation(orientationOption: Int) {
+        val activity = context as? Activity ?: return
+        IntSetting.ORIENTATION_OPTION.int = orientationOption
+        settings.saveSetting(IntSetting.ORIENTATION_OPTION, SettingsFile.FILE_NAME_CONFIG)
+        activity.requestedOrientation = orientationOption
     }
 }

mandarine3ds/mandarine

src/android/app/src/main/java/io/github/mandarine3ds/mandarine/activities/EmulationActivity.kt
@@ -47,6 +47,7 @@ import io.github.mandarine3ds.mandarine.utils.ThemeUtil
 import io.github.mandarine3ds.mandarine.viewmodel.EmulationViewModel
 import io.github.mandarine3ds.mandarine.utils.NetPlayManager
 import io.github.mandarine3ds.mandarine.dialogs.NetPlayDialog
+import io.github.mandarine3ds.mandarine.features.settings.model.IntSetting
 
 class EmulationActivity : AppCompatActivity() {
     private val preferences: SharedPreferences
@@ -79,7 +80,7 @@ class EmulationActivity : AppCompatActivity() {
         NativeLibrary.enableAdrenoTurboMode(BooleanSetting.ADRENO_GPU_BOOST.boolean)
 
         binding = ActivityEmulationBinding.inflate(layoutInflater)
-        screenAdjustmentUtil = ScreenAdjustmentUtil(windowManager, settingsViewModel.settings)
+        screenAdjustmentUtil = ScreenAdjustmentUtil(this, windowManager, settingsViewModel.settings)
         hotkeyUtility = HotkeyUtility(screenAdjustmentUtil, this)
         setContentView(binding.root)
 
@@ -113,6 +114,8 @@ class EmulationActivity : AppCompatActivity() {
 
         isEmulationRunning = true
         instance = this
+
+        applyOrientationSettings() // Check for orientation settings at startup
     }
 
     // On some devices, the system bars will not disappear on first boot or after some
@@ -121,6 +124,7 @@ class EmulationActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         enableFullscreenImmersive()
+        applyOrientationSettings() // Check for orientation settings changes on runtime
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
@@ -228,6 +232,11 @@ class EmulationActivity : AppCompatActivity() {
             controller.systemBarsBehavior =
                 WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
+    }
+
+    private fun applyOrientationSettings() {
+        val orientationOption = IntSetting.ORIENTATION_OPTION.int
+        screenAdjustmentUtil.changeActivityOrientation(orientationOption)
     }
 
     // Gets button presses

src/android/app/src/main/java/io/github/mandarine3ds/mandarine/display/ScreenAdjustmentUtil.kt
@@ -11,9 +11,16 @@ import io.github.mandarine3ds.mandarine.features.settings.model.IntSetting
 import io.github.mandarine3ds.mandarine.features.settings.model.Settings
 import io.github.mandarine3ds.mandarine.features.settings.utils.SettingsFile
 import io.github.mandarine3ds.mandarine.utils.EmulationMenuSettings
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.app.Activity
+import io.github.mandarine3ds.mandarine.R
 
-class ScreenAdjustmentUtil(private val windowManager: WindowManager,
-                           private val settings: Settings) {
+class ScreenAdjustmentUtil(
+    private val context: Context,
+    private val windowManager: WindowManager,
+    private val settings: Settings,
+) {
     fun swapScreen() {
         val isEnabled = !EmulationMenuSettings.swapScreens
         EmulationMenuSettings.swapScreens = isEnabled
@@ -26,9 +33,9 @@ class ScreenAdjustmentUtil(private val windowManager: WindowManager,
     }
 
     fun cycleLayouts() {
-        // TODO: figure out how to pull these from R.array
-        val landscapeValues = intArrayOf(2, 1, 3, 4, 0, 5)
-        val portraitValues = intArrayOf(0, 1)
+        val landscapeValues = context.resources.getIntArray(R.array.landscapeValues)
+        val portraitValues = context.resources.getIntArray(R.array.portraitValues)
+
         if (NativeLibrary.isPortraitMode) {
             val currentLayout = IntSetting.PORTRAIT_SCREEN_LAYOUT.int
             val pos = portraitValues.indexOf(currentLayout)
@@ -54,5 +61,12 @@ class ScreenAdjustmentUtil(private val windowManager: WindowManager,
         settings.saveSetting(IntSetting.SCREEN_LAYOUT, SettingsFile.FILE_NAME_CONFIG)
         NativeLibrary.reloadSettings()
         NativeLibrary.updateFramebuffer(NativeLibrary.isPortraitMode)
+    }
+
+    fun changeActivityOrientation(orientationOption: Int) {
+        val activity = context as? Activity ?: return
+        IntSetting.ORIENTATION_OPTION.int = orientationOption
+        settings.saveSetting(IntSetting.ORIENTATION_OPTION, SettingsFile.FILE_NAME_CONFIG)
+        activity.requestedOrientation = orientationOption
     }
 }