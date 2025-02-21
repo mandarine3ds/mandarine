5c8b3daec3ef99ec1572e6f600006623b7c0331e
Kleidis <167202775+kleidis@users.noreply.github.com>
2025-02-21 19:22:37 +0100

    License headers

--- src/android/app/src/main/java/io/github/mandarine3ds/mandarine/model/GameListItem.kt
+++ src/android/app/src/main/java/io/github/mandarine3ds/mandarine/model/GameListItem.kt
@@ -1,3 +1,7 @@
+// Copyright 2025 Mandarine Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
 package io.github.mandarine3ds.mandarine.model
 
 sealed class GameListItem {
